2486d52aeedce414dfe4227c5560f8ac8c439378
Donald Moore <donmoore26762671@gmail.com>
2018-01-25 11:39:16 -0600

    Java exercise to calculate the circumference of the world.

---
+++ TheWorld.java
@@ -0,0 +1,7 @@
+public class TheWorld {
+  public static void main(String[] args) {
+    System.out.println("The circumference of the world is about " + 4000 + " kilometers");
+    System.out.println("The diameter, circumference divided by PI, equals " + (4000 / 3.14) + " kilometers");
+  
+  }
+}
