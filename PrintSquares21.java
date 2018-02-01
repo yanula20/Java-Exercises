a79016e674e4c4dc7b41fb1f4d0f0218071a237d
Donald Moore <donmoore26762671@gmail.com>
2018-02-01 06:12:56 -0600

    Adding four excercises that print boxes, diamonds, squares using for loops

---
+++ PrintSquares21.java
@@ -0,0 +1,9 @@
+public class PrintSquares21 {
+  public static void main(String[] args) {
+    int count;
+  
+    for (count = -4; count <= 4; count++) {
+      System.out.println("The square root of " + count + " is equal to " + count * count);
+    }
+  }
+}
